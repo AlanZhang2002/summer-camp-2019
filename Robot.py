0084dc8f7b04196d9fd4066caccc14cb3c014cf7
AlanZhang2002 <alanmz2002@gmail.com>
2019-05-21 14:09:04 -0700

    initial commit

---
+++ Robot.py
@@ -0,0 +1,37 @@
+import wpilib
+
+class MyRobot(wpilib.TimedRobot):
+	
+	# Runs once at the beginning, when the robot is turned on
+	def robotInit(self):
+
+
+
+	# Runs once at the beginning of teleop mode
+	def teleopInit(self):
+
+
+
+	# Runs repeatedly every 20ms in teleop mode
+	def teleopPeriodic(self):
+
+
+
+	# Runs once at the beginning of autonomous mode
+	def autonomousInit(self):
+
+
+
+	# Runs repeatedly every 20ms in autonomous mode
+	def autonomousPeriodic(self):
+
+
+
+	# Runs once at the beginning when disabled
+	def disabledInit(self):
+
+
+
+	# Runs repeatedly every 20ms while the robot is disabled
+	def disabledPeriodic(self):
+		# leave blank?
