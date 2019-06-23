c490a3f0b363304e1343eee19da4535abd8095f6
AlanZhang2002 <alanmz2002@gmail.com>
2019-06-23 15:07:44 -0700

    fixed

--- Robot.py
+++ Robot.py
@@ -1,41 +1,32 @@
 import wpilib
-import ctre.WPI_TalonSRX
 
-class MyRobot(wpilib.TimedRobot):
-
-
-	# Runs once at the beginning, when the robot is turned on
+class MyRobot(wpilib.IterativeRobot):
 	def robotInit(self):
-		"""initialize robot parts here"""
-		self.talon = ctre.WPI_TalonSRX(1)
-		self.joystick = wpilib.Joystick(0)
+		"""
+        This function is called upon program startup and
+        should be used for any initialization code.
+        """
+        self.spark = rev.CANSparkMax(1, MotorType.kBrushless) #(deviceID, type)
+        self.joystick = wpilib.Joystick(0)
 
-
-	# Runs once at the beginning of teleop mode
-	def teleopInit(self):
-
-
-
-	# Runs repeatedly every 20ms in teleop mode
-	def teleopPeriodic(self):
-
-
-
-	# Runs once at the beginning of autonomous mode
 	def autonomousInit(self):
+		"""This function is run once each time the robot enters the autonomous mode."""
 
-
-
-	# Runs repeatedly every 20ms in autonomous mode
 	def autonomousPeriodic(self):
+		"""This function is called periodically during autonomous."""
 
+	def teleopInit(self):
+		"""This function is run once each time the robot enters the teleoperated mode."""
 
+	def teleopPeriodic(self):
+		"""This function is called periodically during operator control."""
 
-	# Runs once at the beginning when disabled
 	def disabledInit(self):
+		"""This function is run once each time the robot enters the disabled mode."""
 
-
-
-	# Runs repeatedly every 20ms while the robot is disabled
 	def disabledPeriodic(self):
-		# leave blank?
+		"""This function is called periodically during disabled."""
+
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
