cf98ecad05c70cee8728c00298a414825f84c7be
AlanZhang2002 <alanmz2002@gmail.com>
2019-06-23 16:07:41 -0700

    fix hopefully

--- Robot.py
+++ Robot.py
@@ -1,59 +1,53 @@
 import wpilib
-import ctre.WPI_TalonSRX
+import rev
 
-class MyRobot(wpilib.TimedRobot):
-
-
-	# Runs once at the beginning, when the robot is turned on
+class MyRobot(wpilib.IterativeRobot):
 	def robotInit(self):
-		"""initialize robot parts here"""
-		self.leftTalon1 = ctre.WPI_TalonSRX(15)
-		self.leftTalon2 = ctre.WPI_TalonSRX(14)
-		self.leftTalon3 = ctre.WPI_TalonSRX(13)
-		self.rightTalon1 = ctre.WPI_TalonSRX(20)
-		self.rightTalon2 = ctre.WPI_TalonSRX(1)
-		self.rightTalon3 = ctre.WPI_TalonSRX(2)
+		"""
+		This function is called upon program startup and
+		should be used for any initialization code.
+		"""
+		self.leftSpark1 = rev.CANSparkMax(15, rev.MotorType.kBrushless)
+		self.leftSpark2 = rev.CANSparkMax(14, rev.MotorType.kBrushless)
+		self.leftSpark3 = rev.CANSparkMax(13, rev.MotorType.kBrushless)
+		self.rightSpark1 = rev.CANSparkMax(20, rev.MotorType.kBrushless)
+		self.rightSpark2 = rev.CANSparkMax(1, rev.MotorType.kBrushless)
+		self.rightSpark3 = rev.CANSparkMax(2, rev.MotorType.kBrushless)
 
 		self.joystick1 = wpilib.Joystick(0)
 		self.joystick2 = wpilib.Joystick(1)
 
-		self.leftTalons = wpilib.SpeedControllerGroup(lefttalon1, lefttalon2, lefttalon3)
-		self.rightTalons = wpilib.SpeedControllerGroup(righttalon1, righttalon2, righttalon3)
+		self.leftSparks = wpilib.SpeedControllerGroup(self.leftSpark1, self.leftSpark2, self.leftSpark3)
+		self.rightSparks = wpilib.SpeedControllerGroup(self.rightSpark1, self.rightSpark2, self.rightSpark3)
 
-
-	# Runs once at the beginning of teleop mode
-	def teleopInit(self):
-
-
-
-	# Runs repeatedly every 20ms in teleop mode
-	def teleopPeriodic(self):
-		self.leftTalons.set(self.joystick1.getY() - self.joystick2.getY())
-		self.rightTalons.set(-(self.joystick1.getY() + self.joystick2.getY())) #someone check this, idk if it is right
-
-
-
-	# Runs once at the beginning of autonomous mode
 	def autonomousInit(self):
+		"""This function is run once each time the robot enters the autonomous mode."""
 		self.timer = 0
 
-
-	# Runs repeatedly every 20ms in autonomous mode
 	def autonomousPeriodic(self):
-		self.timer++
+		"""This function is called periodically during autonomous."""
+		self.timer += 1
 		if self.timer < 150:
-			self.leftTalons.set(0.5)
-			self.rightTalons.set(-0.5)
+			self.leftSparks.set(0.5)
+			self.rightSparks.set(-0.5)
 		else:
-			self.leftTalons.set(0)
-			self.rightTalons.set(0)
+			self.leftSparks.set(0)
+			self.rightSparks.set(0)
 
+	def teleopInit(self):
+		"""This function is run once each time the robot enters the teleoperated mode."""
 
-	# Runs once at the beginning when disabled
+	def teleopPeriodic(self):
+		"""This function is called periodically during operator control."""
+		self.leftSparks.set(self.joystick1.getY() - self.joystick2.getY())
+		self.rightSparks.set(-(self.joystick1.getY() + self.joystick2.getY())) #someone check this, idk if it is right
+
 	def disabledInit(self):
+		"""This function is run once each time the robot enters the disabled mode."""
 
-
-
-	# Runs repeatedly every 20ms while the robot is disabled
 	def disabledPeriodic(self):
-		# leave blank?
+		"""This function is called periodically during disabled."""
+
+
+if __name__ == "__main__":
+	wpilib.run(MyRobot)
