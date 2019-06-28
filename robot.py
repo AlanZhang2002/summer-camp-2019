be47416ba43ae144ae4c08e977b66dcc87f3a148
JasonLiu2002 <realjasonliu@gmail.com>
2019-06-27 19:45:28 -0700

    Code deploys

--- robot.py
+++ robot.py
@@ -4,34 +4,38 @@ class MyRobot(wpilib.TimedRobot):
 	
 	# Runs once at the beginning, when the robot is turned on
 	def robotInit(self):
-
+		return
 
 
 	# Runs once at the beginning of teleop mode
 	def teleopInit(self):
-
+		return
 
 
 	# Runs repeatedly every 20ms in teleop mode
 	def teleopPeriodic(self):
-
+		return
 
 
 	# Runs once at the beginning of autonomous mode
 	def autonomousInit(self):
-
+		return
 
 
 	# Runs repeatedly every 20ms in autonomous mode
 	def autonomousPeriodic(self):
-
+		return
 
 
 	# Runs once at the beginning when disabled
 	def disabledInit(self):
-
+		return
 
 
 	# Runs repeatedly every 20ms while the robot is disabled
 	def disabledPeriodic(self):
 		# leave blank?
+		return
+
+if __name__ == "__main__":
+	wpilib.run(MyRobot)
