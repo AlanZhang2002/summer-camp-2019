742e2e8e4ddec15de19f9f8bae10719055521c20
JasonLiu2002 <realjasonliu@gmail.com>
2019-06-29 15:55:07 -0700

    u didnt see this

--- robot.py
+++ robot.py
@@ -22,6 +22,11 @@ class MyRobot(wpilib.IterativeRobot):
 		self.rightSparks = wpilib.SpeedControllerGroup(self.rightSpark1, self.rightSpark2, self.rightSpark3)
 
 		self.leftSpark1.setIdleMode(rev.IdleMode.kBrake)
+		self.leftSpark2.setIdleMode(rev.IdleMode.kBrake)
+		self.leftSpark3.setIdleMode(rev.IdleMode.kBrake)
+		self.rightSpark1.setIdleMode(rev.IdleMode.kBrake)
+		self.rightSpark2.setIdleMode(rev.IdleMode.kBrake)
+		self.rightSpark3.setIdleMode(rev.IdleMode.kBrake)
 
 	def autonomousInit(self):
 		"""This function is run once each time the robot enters the autonomous mode."""
